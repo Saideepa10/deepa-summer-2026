3862fa575375f0b9b265ab0f34c74ae71d0fd199
Deepa <deepalokesha2002@gmail.com>
2026-06-16 12:18:51 +0000

    Basic Programs

--- Basic Coding Programs.c
+++ Basic Coding Programs.c
@@ -4,33 +4,33 @@ void temp_converter()
 {
     double cel, fah, kel, ran;
 
-    printf("Enter the input in celsius:");
+    printf("Enter the input in celsius: ");
     scanf("%lf", &cel);
 
     fah = (cel * 1.8) + 32;
-    printf("celsius to Fahrenheit : %f\n", fah);
+    printf("celsius to Fahrenheit : %f\n",  fah);
 
     kel = cel + 273.15;
-    printf("celsius to kelvin : %f\n", kel);
+    printf("celsius to kelvin : %f\n",  kel);
 
     ran = (cel * 1.8) + 491.67;
-    printf("celsius to Rankine : %f\n", ran);
+    printf("celsius to Rankine : %f\n",  ran);
 }
 
 void even_odd_classifier()
 {
     int num;
 
-    printf("Enter a number:");
+    printf("Enter a number: ");
     scanf("%d", &num);
 
     if(num % 2 == 0)
     {
-        printf("The %d is even", num);
+        printf("The %d is even",  num);
     }
     else
     {
-        printf("The %d is odd", num);
+        printf("The %d is odd",  num);
     }
 }
 
@@ -38,26 +38,25 @@ void grade_calculator()
 {
     int marks;
 
-    printf("Enter the marks:");
+    printf("Enter the marks: ");
+    scanf("%d",&marks);
     if(marks != 0)
     {
-        scanf("%d", &marks);
-
         if(marks >= 90 && marks <= 100)
         {
-            printf("grade A");
+            printf("Grade A");
         }
         else if(marks >= 70 && marks <= 89)
         {
-            printf("grade B");
+            printf("Grade B");
         }
         else if(marks >= 50 && marks <= 69)
         {
-            printf("grade C");
+            printf("Grade C");
         }
         else if(marks >= 35 && marks <= 49)
         {
-            printf("grade D");
+            printf("Grade D");
         }
         else
         {
@@ -74,16 +73,16 @@ void l_year()
 {
     int year;
 
-    printf("Enter a year:");
+    printf("Enter a year: ");
     scanf("%d", &year);
 
     if((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
     {
-        printf("%d is leap year");
+        printf( year, "%d is leap year");
     }
     else
     {
-        printf("%d is not a leap year");
+        printf(year, "%d is not a leap year");
     }
 }
 
@@ -92,28 +91,28 @@ void calculator()
     int a, b;
     char op;
 
-    printf("Enter two numbers a and b:");
+    printf("Enter two numbers a and b: ");
     scanf("%d %d", &a, &b);
 
-    printf("Enter the operator (+,-,*,/):");
+    printf("Enter the operator (+,-,*,/): ");
     scanf(" %c", &op);
 
     switch(op)
     {
         case '+':
-            printf("Addition:%d", a + b);
+            printf("Addition:%d",  a + b);
             break;
 
         case '-':
-            printf("Subtraction:%d", a - b);
+            printf("Subtraction:%d",  a - b);
             break;
 
         case '*':
-            printf("Multiplication:%d", a * b);
+            printf("Multiplication:%d",  a * b);
             break;
 
         case '/':
-            printf("Division:%d", a / b);
+            printf("Division:%d",  a / b);
             break;
     }
 }
@@ -122,10 +121,10 @@ int main()
 {
     int num;
 
-    printf("Enter 1 for temp_converter"
-           "2 for even_odd classifier,"
-           "3 for grade_calculator,"
-           "4 for l_year,"
+    printf("Enter 1 for temp_converter  "
+           "2 for even_odd classifier, "
+           "3 for grade_calculator, "
+           "4 for l_year, "
            "and 5 for calculator");
 
     scanf("%d", &num);
@@ -135,7 +134,6 @@ int main()
         case 1:
             temp_converter();
             break;
-
         case 2:
             even_odd_classifier();
             break;
